86c82bd5dc3003b3aaac3fba5123ef578c51f23e
Ozan Karaali <ozan.karaali@gmail.com>
2017-12-06 09:21:47 +0300

    Init commit

---
+++ park-o-meter.srcs/sources_1/new/Main.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12/06/2017 08:19:10 AM
+-- Design Name: 
+-- Module Name: Main - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Main is
+    Port ( pushButton : in STD_LOGIC_VECTOR (3 downto 0);
+           display : out STD_LOGIC);
+end Main;
+
+architecture Behavioral of Main is
+
+begin
+
+
+end Behavioral;
