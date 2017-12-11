dd1168923db848e2496cee2eb4c91b580169d9be
Ozan Karaali <ozan.karaali@gmail.com>
2017-12-11 15:24:52 +0300

    Implemented time and pushbutton signals. (Synthesis works \o/)

--- park-o-meter.srcs/sources_1/new/Main.vhd
+++ park-o-meter.srcs/sources_1/new/Main.vhd
@@ -1,44 +1,27 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 12/06/2017 08:32:50 AM
--- Design Name: 
--- Module Name: Main - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Main is
-    Port ( Button : in STD_LOGIC_VECTOR (3 downto 0);
-           );
+    Port ( BUTTON : in STD_LOGIC_VECTOR (3 downto 0);
+           mahmut : in STD_LOGIC_VECTOR
+    );
 end Main;
 
 architecture Behavioral of Main is
-signal time: integer <= 0;
+signal time: integer := 0;
 begin
 
+    addCoin: process (BUTTON)
+    begin
+        if(risingedge(BUTTON(0))) then
+            time <= time + 30
+        elsif (risingedge(BUTTON(1))) then
+            time <= time + 60
+        elsif (risingedge(BUTTON(2))) then
+            time <= time + 120
+        elsif (risingedge(BUTTON(3))) then
+            time <= time + 300
+        end if;
+    end process;
 
 end Behavioral;
